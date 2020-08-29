2a22e3ad3946f5c45b29d9764a31636df0c8393f
Alexander96pz <96xanderp@gmail.com>
2020-08-29 12:51:05 -0500

    search completo

--- app.js
+++ app.js
@@ -1,25 +1,24 @@
 $(function () {
     $('#search').keyup(function () {
-        let search = $('#search').val();
-        // console.log(search);
-        $.ajax({
-            url: 'search_task.php',
-            type: 'POST',
-            data: { search },
-            success: function (response) {
-                let tasks = JSON.parse(response);
-                let template = '';
-                tasks.forEach(task => {
-                    template += ` 
+            let search = $('#search').val();
+            $.ajax({
+                url: 'search_task.php',
+                type: 'POST',
+                data: { search },
+                success: function (response) {
+                    let tasks = JSON.parse(response);
+                    let template = '';
+                    tasks.forEach(task => {
+                        template += ` 
                     <tr>
                         <th scope="row">${task.id}</th>
                         <td>${task.title}</td>
                         <td>${task.description}</td>
                         <td>${task.created_at}</td>
                     `;
-                });
-                $('#contenedor_tabla').html(template);
-            }
-        })
+                    });
+                    $('#contenedor_tabla').html(template);
+                }
+            })
     })
 });
